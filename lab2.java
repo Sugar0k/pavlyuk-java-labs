fe7a4e1de161fdf0544e51fc0ae7b0dcc3c3f861
Alexsandr <43041935+Sugar0k@users.noreply.github.com>
2018-10-01 23:28:09 +0000

    Create lab2.java

---
+++ lab2.java
@@ -0,0 +1,16 @@
+
+
+public class lab2 {
+
+    public static void main(String[] args){
+        String str = "Павлюк ИВТ-12";
+        int a = 123;
+        short b = 16;
+        byte c = 28;
+        double d = 4.4321;
+        float f = 4.1231111111f;
+        System.out.printf("Привет,  %s : \na =%5d\nb =%o\nc =%x\nd =%+8.2f\nf =%06.3f", str, a, b, c, d, f);
+    }
+
+
+}
