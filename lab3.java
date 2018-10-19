403071b6b4ea51209c89fd9ef44bef31560f6659
Alexsandr <43041935+Sugar0k@users.noreply.github.com>
2018-10-19 20:15:52 +0300

    Update lab3.java

--- lab3.java
+++ lab3.java
@@ -1,8 +1,25 @@
 public class lab3 {
-
     public static void main(String[] args){
-        int a = 9, b = 2;
+        int a = 2024, b = 341;
         int c =-(a / b + a % b) * 5;
-        System.out.printf("a = %s(2) = %x(16) = %o(8) = %d(10)\nb = %s(2) = %x(16) = %o(8) = %d(10)\na + b = %s(2) = %x(16) = %o(8) = %d(10)\n",Integer.toBinaryString(a), a, a, a, Integer.toBinaryString(b), b, b, b, Integer.toBinaryString(c), c, c, c);
+        System.out.printf("-(a / b + a %% b) * 5 = %s(2) = %x(16) = %o(8) = %d(10)\n", Integer.toBinaryString(c), c, c, c);
+        System.out.printf("a = %s(2) = %x(16) = %o(8) = %d(10)\n", Integer.toBinaryString(a), a, a, a);
+        System.out.printf("b = %s(2) = %x(16) = %o(8) = %d(10)\n", Integer.toBinaryString(b), b, b, b);
+        c = -a;
+        System.out.printf("-a = %s(2) = %x(16) = %o(8) = %d(10)\n", Integer.toBinaryString(c), c, c, c);
+        c = a + b;
+        System.out.printf("a + b = %s(2) = %x(16) = %o(8) = %d(10)\n", Integer.toBinaryString(c), c, c, c);
+        c = a - b;
+        System.out.printf("a - b = %s(2) = %x(16) = %o(8) = %d(10)\n", Integer.toBinaryString(c), c, c, c);
+        c = a * b;
+        System.out.printf("a * b = %s(2) = %x(16) = %o(8) = %d(10)\n", Integer.toBinaryString(c), c, c, c);
+        c = a / b;
+        System.out.printf("a / b = %s(2) = %x(16) = %o(8) = %d(10)\n", Integer.toBinaryString(c), c, c, c);
+        c = a % b;
+        System.out.printf("a %% b = %s(2) = %x(16) = %o(8) = %d(10)\n", Integer.toBinaryString(c), c, c, c);
+        a++;
+        System.out.printf("a++ = %s(2) = %x(16) = %o(8) = %d(10)\n", Integer.toBinaryString(a), a, a, a);
+        b--;
+        System.out.printf("b-- = %s(2) = %x(16) = %o(8) = %d(10)\n", Integer.toBinaryString(b), b, b, b);
     }
 }
