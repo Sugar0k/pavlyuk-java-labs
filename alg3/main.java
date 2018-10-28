4fa2b326e026ecafcf7ace07935a93eafa0bad72
Alexsandr <43041935+Sugar0k@users.noreply.github.com>
2018-10-28 23:56:28 +0300

    Update main.java

--- alg3/main.java
+++ alg3/main.java
@@ -6,11 +6,9 @@ public class main {
 
     public static void face() throws IOException {
         System.out.println("/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
-        System.out.println("addF arg -   Добавить число в начало списка\naddB arg - Добавить число в конец списка");
-        System.out.println("add n arg - Добавить в n по счёту элемент списка элемен arg\ndel n - Удалить n элемент списка");
-        System.out.println("ref - Создаёт новый список из элементов стоящих в обратном порядке");
-        System.out.println("clear - очищает список");
-        System.out.println("print - Вывести лист\nsize - Вывести количество элементов списка\nhelp - Вывести это меню\nexit - Выход");
+        System.out.println("push arg -   Добавить число в начало стек\npop - Взять элемент стека");
+        System.out.println("clear - очищает стек\nchek - Посмотреть элемент стека\nwork - меняет нименьший элемент стека на 0");
+        System.out.println("empty - Проверка на пустоту, если не пувыс выведет стек\nhelp - Вывести это меню\nexit - Выход");
         System.out.println("/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
     }
 
@@ -19,52 +17,50 @@ public class main {
     public static void main(String[] args) throws IOException {
         boolean exit = false;
         face();
-        Stack list = new Stack();
-        Stack testList = new Stack();
+        Stack stack = new Stack();
 
         while (exit == false) {
 
             Scanner jin = new Scanner(System.in);
             String words[] = jin.nextLine().split(" ");
             switch (words[0]){
-                case "get":
-                    System.out.println(list.get());
+                case "work":
+                    if (stack.empty() == false) stack.temp();
+                    stack.printStack();
+                    break;
+                case "empty":
+                    if (stack.empty() == true) System.out.println("Ваш стек пуст");
+                    else  stack.printStack();
+                    break;
+                case "pop":
+                    if (stack.empty() == false) System.out.println(stack.pop());
+                    stack.printStack();
                     break;
                 case "chek":
-                    System.out.println(list.chek());
+                    if (stack.empty() == false) System.out.println(stack.chek());
+                    stack.printStack();
                     break;
                 case "clear":
-                    list.clear();
-                    break;
-                case "ref":
-                    if (list.size() == 0) {
-                        int[] a = new int[list.size()];
-                        System.out.println("Ваш лист пуст");
-                        break;
-                    }
-                    list.copy(testList);
-                    testList.printStack();
+                    stack.clear();
+                    System.out.println("Ваш стек пуст");
                     break;
                 case "5":
-                    list.push(1);
-                    list.push(2);
-                    list.push(3);
-                    list.push(4);
-                    list.push(5);
-                    break;
-                case "print":
-                    list.printStack();
+                    stack.push(1);
+                    stack.push(2);
+                    stack.push(3);
+                    stack.push(4);
+                    stack.push(5);
                     break;
                 case "size":
-                    System.out.println(list.size());
+                    System.out.println(stack.size());
                     break;
                 case "push":
                     if(words.length < 2){
                         System.out.println("Повторите ввод");
                         break;
                     }
-                    list.push(Integer.parseInt(words[1]));
-                    list.printStack();
+                    stack.push(Integer.parseInt(words[1]));
+                    stack.printStack();
                     break;
                 case "help":
                     face();
