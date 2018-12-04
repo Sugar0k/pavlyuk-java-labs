70b933031254a7619f0aabfbd2a1327de2c87ffe
Alexsandr <43041935+Sugar0k@users.noreply.github.com>
2018-12-04 09:50:09 +0300

    final

--- alg5/main.java
+++ alg5/main.java
@@ -5,46 +5,64 @@ public class main {
 
     public static void face(){
         System.out.println("/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
-        System.out.println("push arg -   Добавить число в начало стек\npop - Взять элемент стека");
-        System.out.println("clear - очищает стек\nchek - Посмотреть элемент стека\nwork - меняет нименьший элемент стека на 0");
-        System.out.println("empty - Проверка на пустоту, если не пувыс выведет стек\nhelp - Вывести это меню\nexit - Выход");
+        System.out.println("set key inf - Добавить inf в ячейку с ключём key\ndel key - Очистить ячейку с ключём key");
+        System.out.println("empty key - Находит ячейку по ключу\nhelp - Вывести это меню\nexit - Выход\nprint - Вывод таблицы");
         System.out.println("/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
     }
 
     public  static void main(String[] args){
-
-        hash sh = new hash();
-
-        boolean exit = false;
         Scanner jin = new Scanner(System.in);
+        hash sh;
+        System.out.println("Задайте размер хеш таблицы, но не меньше 20");
+        while (true){
+            int size = jin.nextInt();
+            if (size < 20 ){
+                System.out.println("Повторите ввод");
+                continue;
+            }
+            sh = new hash(size);
+            sh.print();
+            break;
+        }
+        face();
+        boolean exit = false;
         while (exit == false) {
 
             jin = new Scanner(System.in);
             String words[] = jin.nextLine().split(" ");
             switch (words[0]){
-                case "kill":
-                    if (sh.empty(Integer.parseInt(words[1])) == false){
-                        System.out.println("Элемент удалён");
-                        sh.kill(Integer.parseInt(words[1]));
-                    }
-                    else System.out.println("Элемента с таким ключём нет");
-                    break;
-                case "empty":
-                    if (sh.empty(Integer.parseInt(words[1])) == false) System.out.println("Элемент с таким ключём уже есть " + sh.search(Integer.parseInt(words[1])));
-                    else System.out.println("Элемента с таким ключём нет ");
-                    break;
-                case "set":
-                    sh.setCell(Integer.parseInt(words[1]), words[2]);
-                    break;
-                case "show":
-                    sh.show();
-                    break;
-                case "1":
+                case "del":
                     if(words.length < 2){
                         System.out.println("Повторите ввод");
                         break;
                     }
-                    sh.getState(Integer.parseInt(words[1]));
+                    if (sh.empty(Integer.parseInt(words[1])) == false){
+                        System.out.println("Элемент удалён");
+                        sh.del(Integer.parseInt(words[1]));
+                    }
+                    else System.out.println("Элемента с таким ключём нет");
+                    sh.print();
+                    break;
+                case "empty":
+                    if(words.length < 2){
+                        System.out.println("Повторите ввод");
+                        break;
+                    }
+                    if (sh.empty(Integer.parseInt(words[1])) == false) System.out.println("Элемент с таким ключём уже есть ");
+                    else System.out.println("Элемента с таким ключём нет ");
+                    sh.print();
+                    break;
+                case "set":
+                    if(words.length < 3){
+                        System.out.println("Повторите ввод");
+                        break;
+                    }
+                    if (sh.empty(Integer.parseInt(words[1])) == false) System.out.println("Элемент с таким ключём уже есть " + sh.search(Integer.parseInt(words[1])));
+                    else sh.setCell(Integer.parseInt(words[1]), words[2]);
+                    sh.print();
+                    break;
+                case "print":
+                    sh.print();
                     break;
                 case "help":
                     face();
@@ -55,7 +73,6 @@ public class main {
                 default:
                     System.out.println("Повторите ввод");
             }
-
         }
     }
 }
